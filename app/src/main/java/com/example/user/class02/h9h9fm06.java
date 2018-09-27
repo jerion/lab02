d1fc38e256e76df8660d4c1e204278b7cbff86e1
jerion <harry26934719@gmail.com>
2018-09-27 15:30:55 +0800

    Initial commit

---
+++ app/src/main/java/com/example/user/class02/h9h9fm06.java
@@ -0,0 +1,13 @@
+package com.example.user.class02;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class h9h9fm06 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_h9h9fm06);
+    }
+}
